1fcf7e712b1db08a17643c8258d44a5d96d12032
Monirul Islam <monirul.i@imimobile.com>
2017-10-23 22:22:05 +0530

    Proj template.

---
+++ MapperUtility/Mapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapperUtility
+{
+    public class Mapper
+    {
+        public Mapper()
+        {
+
+        }
+
+        static Mapper()
+        {
+
+        }
+
+        public void Map<T1, T2>(T1 first, T2 second) 
+            where T1:IEnumerable<T1>
+            where T2:IComparable<T2>
+        {
+            
+        }
+    }
+}
